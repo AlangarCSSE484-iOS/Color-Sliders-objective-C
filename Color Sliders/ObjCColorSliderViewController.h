62f9485c66f7da2167083b143cc553ef13f80abb
Vibha <alangavr@rose-hulman.edu>
2018-03-20 11:29:11 -0400

    added outlets

--- Color Sliders/ObjCColorSliderViewController.h
+++ Color Sliders/ObjCColorSliderViewController.h
@@ -9,5 +9,19 @@
 #import <UIKit/UIKit.h>
 
 @interface ObjCColorSliderViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UISlider* redSlider;
+@property (weak, nonatomic) IBOutlet UISlider* greenSlider;
+@property (weak, nonatomic) IBOutlet UISlider* blueSlider;
+@property (weak, nonatomic) IBOutlet UISlider* alphaSlider;
+
+@property (weak, nonatomic) IBOutlet UILabel* redLabel;
+@property (weak, nonatomic) IBOutlet UILabel* greenLabel;
+@property (weak, nonatomic) IBOutlet UILabel* blueLabel;
+@property (weak, nonatomic) IBOutlet UILabel* alphaLabel;
+
+@property (weak, nonatomic) IBOutlet UIView* colorView;
+
+
+
 
 @end
