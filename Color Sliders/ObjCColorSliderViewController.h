70ad129fb1da2556ea7378e29dce9bc7a8f6def5
Vibha <alangavr@rose-hulman.edu>
2018-03-20 11:23:52 -0400

    original setup - first commit

---
+++ Color Sliders/ObjCColorSliderViewController.h
@@ -0,0 +1,13 @@
+//
+//  ObjCColorSliderViewController.h
+//  Color Sliders
+//
+//  Created by CSSE Department on 3/20/18.
+//  Copyright © 2018 Rose-Hulman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ObjCColorSliderViewController : UIViewController
+
+@end
---
+++ Color Sliders/ObjCColorSliderViewController.m
@@ -0,0 +1,37 @@
+//
+//  ObjCColorSliderViewController.m
+//  Color Sliders
+//
+//  Created by CSSE Department on 3/20/18.
+//  Copyright © 2018 Rose-Hulman. All rights reserved.
+//
+
+#import "ObjCColorSliderViewController.h"
+
+@interface ObjCColorSliderViewController ()
+
+@end
+
+@implementation ObjCColorSliderViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
